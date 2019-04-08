de9bf6f0cd384d85240797e5862db8433c72e41e
Pavlin Poličar <pavlin.g.p@gmail.com>
2019-04-08 16:26:29 +0200

    Remove VPTree and move query_train into build index function

--- openTSNE/nearest_neighbors.py
+++ openTSNE/nearest_neighbors.py
@@ -1,5 +1,4 @@
 import numpy as np
-from openTSNE.vptree import VPTree as c_vptree
 from sklearn import neighbors
 
 import openTSNE.pynndescent as pynndescent
@@ -15,11 +14,20 @@ class KNNIndex:
         self.n_jobs = n_jobs
         self.random_state = random_state
 
-    def build(self, data):
-        """Build the index so we can query nearest neighbors."""
+    def build(self, data, k):
+        """Build the index so we can query nearest neighbors.
 
-    def query_train(self, data, k):
-        """Query the index for the points used to build index."""
+        Parameters
+        ----------
+        data: array_like
+        k: int
+
+        Returns
+        -------
+        indices: np.ndarray
+        distances: np.ndarray
+
+        """
 
     def query(self, query, k):
         """Query the index with new points."""
@@ -37,7 +45,7 @@ class KNNIndex:
 class BallTree(KNNIndex):
     VALID_METRICS = neighbors.BallTree.valid_metrics
 
-    def build(self, data):
+    def build(self, data, k):
         self.check_metric(self.metric)
         self.index = neighbors.NearestNeighbors(
             algorithm="ball_tree",
@@ -47,51 +55,35 @@ class BallTree(KNNIndex):
         )
         self.index.fit(data)
 
-    def query_train(self, data, k):
-        distances, neighbors = self.index.kneighbors(n_neighbors=k)
-        return neighbors, distances
+        # Return the nearest neighbors in the training set
+        distances, indices = self.index.kneighbors(n_neighbors=k)
+        return indices, distances
 
     def query(self, query, k):
-        distances, neighbors = self.index.kneighbors(query, n_neighbors=k)
-        return neighbors, distances
-
-
-class VPTree(KNNIndex):
-    VALID_METRICS = c_vptree.valid_metrics
-
-    def build(self, data):
-        self.check_metric(self.metric)
-        data = np.ascontiguousarray(data, dtype=np.float64)
-        self.index = c_vptree(data)
-
-    def query_train(self, data, k):
-        indices, distances = self.index.query_train(k + 1, num_threads=self.n_jobs)
-        return indices[:, 1:], distances[:, 1:]
-
-    def query(self, query, k):
-        query = np.ascontiguousarray(query, dtype=np.float64)
-        return self.index.query(query, k, num_threads=self.n_jobs)
+        distances, indices = self.index.kneighbors(query, n_neighbors=k)
+        return indices, distances
 
 
 class NNDescent(KNNIndex):
     VALID_METRICS = pynndescent.distances.named_distances
 
-    def build(self, data):
+    def build(self, data, k):
         self.check_metric(self.metric)
-
-        # These values were taken from UMAP, which we assume to be sensible defaults
-        n_trees = 5 + int(round((data.shape[0]) ** 0.5 / 20))
-        n_iters = max(5, int(round(np.log2(data.shape[0]))))
 
         if self.metric_params is None:
             metric_kwds = {}
         else:
             metric_kwds = self.metric_params
 
+        # These values were taken from UMAP, which we assume to be sensible defaults
+        n_trees = 5 + int(round((data.shape[0]) ** 0.5 / 20))
+        n_iters = max(5, int(round(np.log2(data.shape[0]))))
+
         # UMAP uses the "alternative" algorithm, but that sometimes causes
         # memory corruption, so use the standard one, which seems to work fine
         self.index = pynndescent.NNDescent(
             data,
+            n_neighbors=k + 1,
             metric=self.metric,
             metric_kwds=metric_kwds,
             random_state=self.random_state,
@@ -101,9 +93,8 @@ class NNDescent(KNNIndex):
             max_candidates=60,
         )
 
-    def query_train(self, data, k):
-        neighbors, distances = self.index.query(data, k=k + 1, queue_size=1)
-        return neighbors[:, 1:], distances[:, 1:]
+        indices, distances = self.index._neighbor_graph
+        return indices[:, 1:], distances[:, 1:]
 
     def query(self, query, k):
         return self.index.query(query, k=k, queue_size=1)
